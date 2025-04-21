24b6a54f82e9d92fe193fed152e47b474b479a4a
MaybeDH <hasselc081@gmail.com>
2025-04-21 09:13:47 -0400

    refactor(products): migrar de fetch a axios y añadir notificaciones con react-hot-toast

--- app/(dashboard)/products/page.js
+++ app/(dashboard)/products/page.js
@@ -1,13 +1,10 @@
 import { ProductManager } from "@/components/products/product-manager";
+import { api } from "@/lib/axios";
 
 export default async function Page() {
-  const resp = await fetch("http://localhost:8000/products/", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await resp.json();
+
+  const resp = await api.get("/products/");
+  const data = resp.data;
 
   return (
     <div className="container mx-auto p-4 md:p-6 lg:p-8 overflow-x-auto">
