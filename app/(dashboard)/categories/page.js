3c3eaf0985dfb7dca44d03a6f926b56a80efaaf1
MaybeDH <hasselc081@gmail.com>
2025-04-21 12:36:23 -0400

    refactor(categories): migrar de fetch a axios y añadir notificaciones con react-hot-toast

--- app/(dashboard)/categories/page.js
+++ app/(dashboard)/categories/page.js
@@ -1,14 +1,9 @@
 import { CategoryManager } from "@/components/categories/category-manager";
+import { api } from "@/lib/axios";
 
 export default async function CategoriesPage() {
-  const resp = await fetch("http://localhost:8000/catalog/products_category/", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
 
-  const data = await resp.json();
+  const {data} = await api.get("/catalog/products_category/");
   return (
     <div className="container mx-auto p-4 md:p-6 lg:p-8 overflow-x-auto">
       <CategoryManager allCategories={data} />
